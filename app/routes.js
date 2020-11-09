2ad73c12b2cbd49b50e836aeb718bb79f32aecae
ritesh kumar <riteshmsec@gmail.com>
2020-11-09 16:41:52 +0530

    modification done to static

--- app/routes.js
+++ app/routes.js
@@ -54,5 +54,5 @@ module.exports = function (app) {
     // app.get('*', function (req, res) {
     //     res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     // });
-    app.get('/',"helleos")
+    app.get('*', res.sendFile("hello"))
 };
